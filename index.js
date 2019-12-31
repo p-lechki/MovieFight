5a0599351e0da503ee029b7c72c6d86fe330c4c1
Paweł Lechki <p.lechki@icloud.com>
2019-12-31 01:37:51 +0100

    Add input dropdown  widget

--- index.js
+++ index.js
@@ -6,25 +6,55 @@ const fetchData = async (searchTerm) => {
 		}
 	});
 
-	console.log(response.data);
+	if (response.data.Error) {
+		return [];
+	}
+
+	return response.data.Search;
 };
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
-const debounce = (func, delay = 1000) => {
-	let timeoutId;
-	return (...args) => {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
-		timeoutId = setTimeout(() => {
-			func.apply(null, args);
-		}, delay);
-	};
-};
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
 
-const onInput = (event) => {
-	fetchData(event.target.value);
+	if (!movies.length) {
+		dropdown.classList.remove('is-active');
+		return;
+	}
+
+	resultsWrapper.innerHTML = '';
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
+
+		resultsWrapper.appendChild(option);
+	}
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (event) => {
+	if (!root.contains(event.target)) {
+		dropdown.classList.remove('is-active');
+	}
+});
